c90c1951cbc8463659ad4b248db2e596c1d89831
BogdanLi <li.bogdan44@gmail.com>
2024-08-02 11:49:15 +0500

    Added folders and file icons

---
+++ components/UI/AboutPage/FileLink.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import js from "@/assets/icons/js.svg";
+import tsx from "@/assets/icons/tsx.svg";
+import { useEffect, useState } from "react";
+
+interface Props {
+  href: string;
+  label: string;
+}
+
+const getFormat = (label: string) => {};
+
+const FileLink = ({ href, label }: Props) => {
+  const [img, setImg] = useState("");
+
+  return (
+    <Link href={href} className="flex items-center gap-2 py-2">
+      <Image
+        src={label.endsWith(".tsx") ? tsx : js}
+        alt=""
+        width={16}
+        height={16}
+      />
+      {label}
+    </Link>
+  );
+};
+
+export default FileLink;
